999ac3e5d2058a8845ddea6247e197fb5572a0ac
Gabriel Boyer <gboyer@gmail.com>
2024-08-24 11:54:53 -0700

    Update resume with latest brands

--- index.html.pm
+++ index.html.pm
@@ -1,5 +1,6 @@
 #lang pollen
 ◊(require pollen/tag)
+
 ◊(define root (default-tag-function 'article #:id "resume"))
 
 ◊h1{Gabriel Boyer}
@@ -7,7 +8,7 @@
 
 ◊section[#:id "intro"]{
     ◊p{
-        I'm a soft­ware en­gin­eer with ex­per­ience build­ing web app­lica­tions and ser­v­ices. You can con­tact me by ◊a[#:href "mailto:{{ site.email }}" #:title "{{ site.email }}" #:rel "me" #:class "print-title"]{email}, or on ◊a[#:href "http://www.linkedin.com/in/gabrielboyer" #:title "http://www.linkedin.com/in/gabrielboyer" #:rel "me" #:class "print-title"]{LinkedIn}.
+        I'm a soft­ware en­gin­eer with ex­per­ience build­ing web app­lica­tions and ser­v­ices. You can con­tact me by ◊email-link{email}, or on ◊linkedin-link{LinkedIn}.
     }
 }
 
@@ -16,7 +17,7 @@
 
     ◊h3{Microsoft Corporation (2016–)}
     ◊p{
-        I'm cur­rent­ly a Prin­ci­pal Soft­ware En­gi­neer◊sidenote[#:id "ms-pse"]{2019–} work­ing on the ◊a[#:href "https://powerapps.microsoft.com/en-us/"]{Pow­er Apps} client in­fra­struc­ture. I was pre­vi­ous­ly a Se­nior Soft­ware En­gi­neer­ing Man­ag­er◊sidenote[#:id "ms-sem"]{2017–2018} work­ing on the ◊a[#:href "https://dynamics.microsoft.com/en-us/"]{Dy­nam­ics 365} client in­fra­struc­ture, and I start­ed at Mi­crosoft as a Se­nior Soft­ware En­gi­neer◊sidenote[#:id "ms-sse"]{2016–2017} work­ing on Dy­nam­ics 365 Por­tals (which is now ◊a[#:href "https://powerapps.microsoft.com/en-us/portals/"]{Pow­er Apps Por­tals}).
+        I'm cur­rent­ly a Prin­ci­pal Soft­ware En­gi­neer◊sidenote[#:id "ms-pse"]{2019–} work­ing on ◊a[#:href "https://powerapps.microsoft.com/en-us/"]{Pow­er Apps}. I was pre­vi­ous­ly a Se­nior Soft­ware En­gi­neer­ing Man­ag­er◊sidenote[#:id "ms-sem"]{2017–2018} work­ing on the ◊a[#:href "https://dynamics.microsoft.com/en-us/"]{Dy­nam­ics 365} client in­fra­struc­ture, and I start­ed at Mi­crosoft as a Se­nior Soft­ware En­gi­neer◊sidenote[#:id "ms-sse"]{2016–2017} work­ing on Dy­nam­ics 365 Por­tals (which is now ◊a[#:href "https://www.microsoft.com/en-us/power-platform/products/power-pages"]{Pow­er Pages}).
     }
 
     ◊h3{Adxstudio Inc. (2004–2016)}
@@ -32,3 +33,18 @@
         I earned a B.Sc. in Com­put­er Sci­ence.
     }
 }
+
+◊(define email-link
+    (default-tag-function 'a
+        #:href "mailto:{{ site.email }}"
+        #:title "{{ site.email }}"
+        #:rel "me"
+        #:class "print-title"))
+
+◊(define linkedin-link
+    (let ([linkedin-url "https://www.linkedin.com/in/gabrielboyer"])
+        (default-tag-function 'a
+            #:href linkedin-url
+            #:title linkedin-url
+            #:rel "me"
+            #:class "print-title")))
