329d47d7d4bae1db4b7eaa072d2240f9d469de73
Gabriel Boyer <gboyer@gmail.com>
2024-08-24 11:57:57 -0700

    Update resume details

--- index.html.pm
+++ index.html.pm
@@ -17,7 +17,7 @@
 
     ◊h3{Microsoft Corporation (2016–)}
     ◊p{
-        I'm cur­rent­ly a Prin­ci­pal Soft­ware En­gi­neer◊sidenote[#:id "ms-pse"]{2019–} work­ing on ◊a[#:href "https://powerapps.microsoft.com/en-us/"]{Pow­er Apps}. I was pre­vi­ous­ly a Se­nior Soft­ware En­gi­neer­ing Man­ag­er◊sidenote[#:id "ms-sem"]{2017–2018} work­ing on the ◊a[#:href "https://dynamics.microsoft.com/en-us/"]{Dy­nam­ics 365} client in­fra­struc­ture, and I start­ed at Mi­crosoft as a Se­nior Soft­ware En­gi­neer◊sidenote[#:id "ms-sse"]{2016–2017} work­ing on Dy­nam­ics 365 Por­tals (which is now ◊a[#:href "https://www.microsoft.com/en-us/power-platform/products/power-pages"]{Pow­er Pages}).
+        I'm cur­rent­ly a Prin­ci­pal Soft­ware En­gi­neer◊sidenote[#:id "ms-pse"]{2019–} work­ing on ◊a[#:href "https://www.microsoft.com/en-us/power-platform"]{Pow­er Platform}. I was pre­vi­ous­ly a Se­nior Soft­ware En­gi­neer­ing Man­ag­er◊sidenote[#:id "ms-sem"]{2017–2018} work­ing on the ◊a[#:href "https://dynamics.microsoft.com/en-us/"]{Dy­nam­ics 365} client in­fra­struc­ture, and I start­ed at Mi­crosoft as a Se­nior Soft­ware En­gi­neer◊sidenote[#:id "ms-sse"]{2016–2017} work­ing on Dy­nam­ics 365 Por­tals (which is now ◊a[#:href "https://www.microsoft.com/en-us/power-platform/products/power-pages"]{Pow­er Pages}).
     }
 
     ◊h3{Adxstudio Inc. (2004–2016)}
